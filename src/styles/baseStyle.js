783c3a5b6234ce898d8588dadedfc90847f0b247
王松 <wangsong1299@163.com>
2020-10-09 19:59:52 +0800

    feat: header优化

---
+++ src/styles/baseStyle.js
@@ -0,0 +1,13 @@
+export const baseStyle = {
+    header:{
+        width: '1920px',
+        left: 0,
+        right: 0,
+        marginleft: 'auto',
+        marginRight: 'auto',
+        height: '120px',
+    },
+    rowFlex: {
+        display: 'flex'
+    }
+}
