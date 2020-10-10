f41f35f1f4fb41fceb25de835a5b7005655cb376
ftc <ggg65437@163.com>
2020-10-10 09:46:41 +0800

    分支创建

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,7 @@ import { Route, Redirect } from 'react-router'
 import { HashRouter, Switch } from 'react-router-dom'
 
 import Home from 'routes/home/home'
+import Practice from 'routes/practice/practice'
 
 const Routes = () => (
   <HashRouter>
@@ -10,6 +11,7 @@ const Routes = () => (
       <Route exact path="/" render={() => <Redirect to="/home" />} />
       <Switch>
         <Route path="/home" component={Home} />
+        <Route path="/practice" component={Practice} />
       </Switch>
     </div>
   </HashRouter>
