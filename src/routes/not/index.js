bbe541997b571f50e18e550f0ce2bfc63d649fcc
wangsong <wangsong1299@163.com>
2020-10-11 22:03:08 +0800

    feat: 代码优化

--- src/routes/not/index.js
+++ src/routes/not/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Button} from 'antd'
+import Header from 'components/Header.js'
 
 const Not = (props={}) => {
     const back = ()=> {
@@ -9,6 +10,7 @@ const Not = (props={}) => {
     }
     return (
         <div className="App">
+            <Header props={props} />
             <div style={{position:"fixed",left:"50%",top:"50%",transform:"translate(-50%,-50%)"}}>
                 <div style={{fontSize:60,color:'#0099ff',height:160}}>即将推出   敬请期待</div>
                 <Button onClick={back} style={{fontSize:30,height:60,color:'#0099ff',borderColor:"#0099ff"}}>返回首页</Button>
