28dc62164d9d31a5b8044859612fae3b629940c5
ftc <ggg65437@163.com>
2020-10-30 15:14:24 +0800

    拆分

--- src/routes/intelligence/courseDetail/components/DetailBase.js
+++ src/routes/intelligence/courseDetail/components/DetailBase.js
@@ -2,13 +2,13 @@
 import React from "react";
 
 const DetailBase = (props={}) => {
-    const {classBase} = props;
-    return (
-        <div>
-            <div style={{ fontSize: '20px', fontWeight: '700' }}>简介：{classBase.title}</div>
-            <div style={{ fontSize: '18px', }}>内容：{classBase.content}</div>
-        </div>
-    );
+  const {classBase} = props;
+  return (
+    <div>
+      <div style={{ fontSize: '20px', fontWeight: '700' }}>简介：{classBase.title}</div>
+      <div style={{ fontSize: '18px', }}>内容：{classBase.content}</div>
+    </div>
+  );
 };
 
 export default DetailBase;
--- src/routes/intelligence/courseDetail/components/DetailContact.js
+++ src/routes/intelligence/courseDetail/components/DetailContact.js
@@ -2,15 +2,15 @@
 import React from "react";
 
 const DetailBase = (props = {}) => {
-    const { teacher } = props;
-    return (
-        <div style={{ textAlign: 'left', backgroundColor: "#dcdcdc", marginTop: '20px', fontSize: '16px' }}>
-        <div style={{ padding: '10px 20px' }}>联系方式</div>
-        <div style={{ padding: '10px 20px' }}>{teacher.name}老师emails：{teacher.email}</div>
-        <div style={{ padding: '10px 20px' }}>助教老师emails：{teacher.supEmail}</div>
-        <div style={{ padding: '10px 20px' }}>微信号：{teacher.wechat}</div>
-      </div>
-    );
+  const { teacher } = props;
+  return (
+    <div style={{ textAlign: 'left', backgroundColor: "#dcdcdc", marginTop: '20px', fontSize: '16px' }}>
+      <div style={{ padding: '10px 20px' }}>联系方式</div>
+      <div style={{ padding: '10px 20px' }}>{teacher.name}老师emails：{teacher.email}</div>
+      <div style={{ padding: '10px 20px' }}>助教老师emails：{teacher.supEmail}</div>
+      <div style={{ padding: '10px 20px' }}>微信号：{teacher.wechat}</div>
+    </div>
+  );
 };
 
 export default DetailBase;
